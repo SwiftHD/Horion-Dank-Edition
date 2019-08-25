96aeaa7853680f7ebf2249028c4d0c788fee5b85
SwiftHD <jyetton1@outlook.com>
2019-08-25 13:30:32 +0100

    Add files via upload

---
+++ Horion/GuiUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+#include"DrawUtils.h"
+
+class GuiUtils
+{
+public:
+	static void drawCrossLine(vec2_t pos, MC_Color col, float lineWidth, float crossSize, bool secondCross);
+	static void checkBox();
+};
+
